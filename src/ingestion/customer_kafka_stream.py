636b338c1b43bc5e5ebf2514fe5337cbb6303a3a
Prajeesh Chavan <prajeeshchavan@gmail.com>
2026-07-01 17:41:46 +0530

    feat(ingest): implement customer Kafka ingestion

---
+++ src/ingestion/customer_kafka_stream.py
@@ -0,0 +1,116 @@
+from pathlib import Path
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+# ----------------------------
+# Spark Session
+# ----------------------------
+spark = (
+    SparkSession.builder
+    .appName("Customer Stream")
+    .config(
+        "spark.jars.packages",
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1"
+    )
+    .config("spark.sql.streaming.schemaInference", "true")
+    .config("spark.sql.session.timeZone", "UTC")
+    .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("WARN")
+
+
+# ----------------------------
+# Set Configurations
+# ----------------------------
+KAFKA_TOPIC = "telemetry.ecommerce.customers"
+BOOTSTRAP_SERVERS = "localhost:9092"
+
+
+
+# ----------------------------
+# Schema
+# ----------------------------
+customer_schema = StructType([
+    StructField("created_at", StringType()),
+    StructField("customer_id", StringType()),
+    StructField("customer_status", StringType()),
+    StructField("email", StringType()),
+    StructField("first_name", StringType()),
+    StructField("is_deleted", StringType()),
+    StructField("last_name", StringType()),
+    StructField("phone", StringType()),
+    StructField("registered_at", StringType()),
+    StructField("updated_at", StringType())
+])
+
+
+# ----------------------------
+# Read from Kafka
+# ----------------------------
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", BOOTSTRAP_SERVERS) \
+    .option("subscribe", KAFKA_TOPIC) \
+    .load()
+
+
+# ----------------------------
+# Parse JSON
+# ----------------------------
+json_df = df.selectExpr("CAST(value AS STRING) as value")
+
+parsed_df = json_df.select(
+    from_json(col("value"), customer_schema).alias("data")
+).select("data.*")
+
+
+# ----------------------------
+# Transformations (clean layer)
+# ----------------------------
+clean_df = parsed_df \
+    .withColumn("created_at", to_timestamp("created_at")) \
+    .withColumn("registered_at", to_timestamp("registered_at")) \
+    .withColumn("updated_at", to_timestamp("updated_at")) \
+    .withColumn("ingest_time", current_timestamp()) \
+    .withColumn("email", lower(col("email"))) \
+    .withColumn(
+        "full_name",
+        concat_ws(" ", col("first_name"), col("last_name"))
+    ) \
+    .withColumn(
+        "is_deleted",
+        col("is_deleted").cast("boolean")
+    ) \
+    .filter(col("is_deleted") == False)
+
+
+# ----------------------------
+# Output Paths
+# ----------------------------
+PROJECT_ROOT = Path.cwd()
+
+OUTPUT_PATH = PROJECT_ROOT  / "Data" / "bronze" / "customers"
+CHECKPOINT_PATH = PROJECT_ROOT / "Data" / "checkpoints" / "customers"
+
+
+# ----------------------------
+# Write Stream (Parquet)
+# ----------------------------
+query = clean_df.writeStream \
+    .format("parquet") \
+    .option("path", OUTPUT_PATH) \
+    .option("checkpointLocation", CHECKPOINT_PATH) \
+    .outputMode("append") \
+    .start()
+
+
+# ----------------------------
+# Monitor
+# ----------------------------
+print("Customers stream started...")
+
+query.awaitTermination()
+
