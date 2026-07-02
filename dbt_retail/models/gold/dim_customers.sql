8cfa945ff7b39128f2af1e588c4a2849dd1a07b6
Prajeesh Chavan <prajeeshchavan@gmail.com>
2026-07-02 16:40:44 +0530

    feat(dbt): change to incremental approach

--- dbt_retail/models/gold/dim_customers.sql
+++ dbt_retail/models/gold/dim_customers.sql
@@ -1,10 +1,16 @@
 {{ config(
-    materialized='table',
-    unique_key='customer_id'
+    materialized='incremental',
+    unique_key='customer_id',
+    incremental_strategy='merge'
 ) }}
 
 WITH source_silver AS (
     SELECT * FROM {{ source('snowflake_silver', 'customers') }}
+    
+    {% if is_incremental() %}
+      -- This filter ensures Snowflake only scans rows updated since the last dbt run
+      WHERE ingest_time > (SELECT MAX(silver_ingest_time) FROM {{ this }})
+    {% endif %}
 ),
 
 gold_transformations AS (
