b07fa6b6b2aa114089a2d7941225cf11b1d6c553
Prajeesh Chavan <prajeeshchavan@gmail.com>
2026-07-05 12:35:15 +0530

    fix(spark): suppress Ivy noise via PYSPARK_GATEWAY_OPTS before JVM start

--- src/common/spark.py
+++ src/common/spark.py
@@ -1,17 +1,20 @@
+import os
 from pyspark.sql import SparkSession
 
+
 def create_spark_session(app_name: str) -> SparkSession:
+    os.environ["PYSPARK_GATEWAY_OPTS"] = "-Divy.log=ERROR"
     spark = (
         SparkSession.builder
         .appName(app_name)
-    .config(
-        "spark.jars.packages",
-        ",".join([
-            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1",
-            "net.snowflake:spark-snowflake_2.12:3.1.9",
-            "net.snowflake:snowflake-jdbc:3.24.2"
-        ])
-    )
+        .config(
+            "spark.jars.packages",
+            ",".join([
+                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1",
+                "net.snowflake:spark-snowflake_2.12:3.1.9",
+                "net.snowflake:snowflake-jdbc:3.24.2"
+            ])
+        )
         .config("spark.sql.streaming.schemaInference", "true")
         .config("spark.sql.session.timeZone", "UTC")
         .config("spark.driver.host", "127.0.0.1")
