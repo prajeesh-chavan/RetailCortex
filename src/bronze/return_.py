61e9fb50432834088022de93c457f6bb4e999cc4
Prajeesh Chavan <prajeeshchavan@gmail.com>
2026-07-07 08:47:55 +0530

    feat(returns): remove old return_ files after rename

--- src/bronze/return_.py
+++
@@ -1,9 +0,0 @@
-from src.bronze.runner import run_bronze_pipeline
-from src.schemas.return_schema import RETURN_SCHEMA
-
-if __name__ == "__main__":
-    run_bronze_pipeline(
-        entity="returns",
-        kafka_topic="telemetry.ecommerce.returns",
-        schema=RETURN_SCHEMA
-    )
